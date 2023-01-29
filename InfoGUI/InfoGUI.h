708e9e276bb99a7a84db2d6ff81dae85ec8e13ac
Aaron X <aarondigital@gmail.com>
2023-01-29 09:20:52 +0800

    Added SDL/Dear Imgui GUI demo

---
+++ InfoGUI/InfoGUI.h
@@ -0,0 +1,15 @@
+#pragma once
+#define SDL_MAIN_HANDLED
+
+#include <iostream>
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdlrenderer.h"
+#include <stdio.h>
+#include <SDL.h>
+
+#if !SDL_VERSION_ATLEAST(2,0,17)
+#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
+#endif
+
